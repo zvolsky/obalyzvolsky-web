37bb5c3c9e7bfb9d5a83a9b55232dea7a9d4966c
Mirek Zvolský <zvolsky@seznam.cz>
2020-06-01 17:39:53 +0200

    pokus

--- api/paperboy.js
+++ api/paperboy.js
@@ -1,20 +1,6 @@
-// use the .env if present, do nothing otherwise
-require('dotenv').config();
-// the utility to handle StaticEmail requests
-const {create} = require('static.email.ses');
-// secrets revealed via dotenv or now
-const {
-  AWS_SES_TO: to,
-  AWS_SES_REGION: region,
-  AWS_SES_ACCESS_KEY: accessKeyId,
-  AWS_SES_SECRET_KEY: secretAccessKey
-} = process.env;
-// the serverless function that now will use
-module.exports = create({
-  site: 'obalyzvolsky.cz',  // put your domain here
-  sender: 'kontakt-no-reply@obalyzvolsky.cz', // any sender name
-  to,
-  region,
-  accessKeyId,
-  secretAccessKey
-});
+module.exports = (request, response) => {
+  if (request.method === 'POST')
+    response.status(200).send('OK');
+  else
+    response.status(400).send('Bad Request');
+};
